f63b15bf8b28d0050f6327e2dc7be43b40903da7
50-Course <eridotdev@gmail.com>
2025-04-30 00:49:32 +0100

    feat: implement guardrails to run migrations only if `DATABASE_URL` is configured

--- tokenscanner/scripts/runserver.sh
+++ tokenscanner/scripts/runserver.sh
@@ -7,6 +7,16 @@ if [ -f /app/src/.env ]; then
   export $(cat /app/src/.env | xargs)
 fi
 
+# ensure DATABASE_URL is set
+if [ -z "${DATABASE_URL:-}" ]; then
+  echo "ERROR: DATABASE_URL is not set. Please set it before running this script."
+  exit 1
+fi
+
+# we migrate
+/app/src/migrate.sh
+
+# finally, we run the server
 exec uvicorn src.main:app \
   --host 0.0.0.0 \
   --port 8000 \
