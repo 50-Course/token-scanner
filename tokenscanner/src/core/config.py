800a541e44932274bff84a58676c674f572030ba
50-Course <eridotdev@gmail.com>
2025-04-30 14:03:09 +0100

    docs: OpenAPI documentation updates

--- tokenscanner/src/core/config.py
+++ tokenscanner/src/core/config.py
@@ -22,8 +22,6 @@ class Settings(BaseSettings):  # type: ignore
     PROJECT_NAME: str = "Token Scanner"
     PROJECT_VERSION: str = "1.0.0"
 
-    # CORS_ALLOWED_ORIGINS: list[str] = []
-
     ENVIRONMENT: Literal["local", "production"] = os.getenv("ENVIRONMENT", "local")
     SENTRY_DSN: Optional[HttpUrl] = None
 
--- tokenscanner/src/main.py
+++ tokenscanner/src/main.py
@@ -7,6 +7,27 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(
     debug=settings.DEBUG_ENABLED if settings.DEBUG_ENABLED else False,
     title=settings.PROJECT_NAME,
+    redoc_url="/api/docs",
+    docs_url="",
+    servers=[
+        {
+            "url": settings.SERVER_HOST,
+            "description": settings.SERVER_NAME,
+        },
+        {
+            "url": "http://104.154.104.188:8000",
+            "description": settings.ENVIRONMENT,
+        },
+    ],
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://github.com/50-Course/token-scanner/blob/main/LICENSE",
+    },
+    contact={
+        "name": "Eri A. (50-Course)",
+        "url": "https://github.com/50-Course",
+        "email": "eridotdev@proton.me",
+    },
     summary=settings.PROJECT_DESCRIPTION,
     version="1.0.0",
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
@@ -16,7 +37,6 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 
 if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
     sentry_sdk.init(dsn=str(settings.SENTRY_DSN), enable_tracing=True)
-
 
 app.add_middleware(
     CORSMiddleware,
